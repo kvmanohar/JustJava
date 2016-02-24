b09a1566cc2796c161b73a7c13c908d4d047e877
Manohar <kvmanohar@gmail.com>
2016-02-24 21:21:06 +0000

    - Made few more updates to String resources and replaced all strings in the code and XML

--- app/src/main/java/com/example/manohar_kurapati/justjava/MainActivity.java
+++ app/src/main/java/com/example/manohar_kurapati/justjava/MainActivity.java
@@ -10,12 +10,14 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.text.NumberFormat;
+
 /**
  * This app displays an order form to order coffee.
  */
 public class MainActivity extends AppCompatActivity {
 
-    int quantity = 0;
+    int quantity = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,26 +36,26 @@ public class MainActivity extends AppCompatActivity {
         String orderSummary;
 
         CheckBox checkBoxWhipCream = (CheckBox) findViewById(R.id.whipping_cream_checkbox);
-        if (checkBoxWhipCream.isChecked())  creamAmount = 5;
+        if (checkBoxWhipCream.isChecked()) creamAmount = 5;
 
         CheckBox checkBoxChocolate = (CheckBox) findViewById(R.id.add_chocolate_checkbox);
-        if (checkBoxChocolate.isChecked())  chocAmount = 2;
+        if (checkBoxChocolate.isChecked()) chocAmount = 2;
 
         EditText nameField = (EditText) findViewById(R.id.name_field);
         name = nameField.getText().toString();
 
-        if (name.length() == 0){
-            Toast.makeText(this,"Enter name to continue",Toast.LENGTH_SHORT).show();
+        if (name.length() == 0) {
+            Toast.makeText(this, getString(R.string.enter_your_name), Toast.LENGTH_SHORT).show();
             return;
         }
 
-        orderSummary=calculatePrice(quantity,creamAmount,chocAmount,name);
+        orderSummary = calculatePrice(quantity, creamAmount, chocAmount, name);
 //        displayMessage(orderSummary);
 
         Intent intent = new Intent(Intent.ACTION_SENDTO);
         intent.setData(Uri.parse("mailto:sheetalkurapati@gmail.com")); // only email apps should handle this
         intent.putExtra(Intent.EXTRA_SUBJECT, "Just JAVA Order for " + name);
-        intent.putExtra(Intent.EXTRA_TEXT,orderSummary);
+        intent.putExtra(Intent.EXTRA_TEXT, orderSummary);
 
         if (intent.resolveActivity(getPackageManager()) != null) {
             startActivity(intent);
@@ -75,7 +77,7 @@ public class MainActivity extends AppCompatActivity {
      */
     public void incrementQuantity(View view) {
         if (quantity == 100) {
-            Toast.makeText(this,"You cannot have more than 100 coffees", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "You cannot have more than 100 coffees", Toast.LENGTH_SHORT).show();
             return;
         }
         quantity = quantity + 1;
@@ -87,7 +89,7 @@ public class MainActivity extends AppCompatActivity {
      */
     public void decrementQuantity(View view) {
         if (quantity == 1) {
-            Toast.makeText(this,"You cannot have less than 1 coffee",Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "You cannot have less than 1 coffee", Toast.LENGTH_SHORT).show();
             return;
         }
         quantity = quantity - 1;
@@ -100,29 +102,33 @@ public class MainActivity extends AppCompatActivity {
     private void display(int number) {
         TextView quantityTextView = (TextView) findViewById(
                 R.id.quantity_text_view);
-        quantityTextView.setText("" + number);
+        quantityTextView.setText(getString(R.string.display_quantity, number));
     }
 
     /**
      * This method calculates the Final price and prepares the message to display
-     * @param quantity of coffee ordered
+     *
+     * @param quantity    of coffee ordered
      * @param creamAmount Whipping cream amount
-     * @param chocAmount Chocolate amount if selected by user
+     * @param chocAmount  Chocolate amount if selected by user
      * @return String
      */
-    public String calculatePrice(int quantity, int creamAmount, int chocAmount, String name){
+    private String calculatePrice(int quantity, int creamAmount, int chocAmount, String name) {
 
-        String summaryMessage = "Name : " + name;
+        int totalAmount = (quantity * 5) + creamAmount + chocAmount;
 
-        int totalAmount = (quantity * 5 ) + creamAmount + chocAmount;
-        summaryMessage = summaryMessage + "\nQuantity : " + quantity;
-        summaryMessage = summaryMessage + "\nTotal : £ " + totalAmount;
-        summaryMessage = summaryMessage + "\n";
+        String summaryMessage = getString(R.string.order_summary_name, name);
+        summaryMessage += "\n\n" + getString(R.string.order_summary_quantity, quantity);
 
-        if (creamAmount > 0) summaryMessage = summaryMessage + "\nAdded Whipped Cream : £5";
-        if (chocAmount > 0) summaryMessage = summaryMessage + "\nAdded Chocolate : £2";
+        if (creamAmount > 0) {
+            summaryMessage += "\n" + getString(R.string.order_summary_whipped, NumberFormat.getCurrencyInstance().format(creamAmount));
+        }
+        if (chocAmount > 0) {
+            summaryMessage += "\n" + getString(R.string.order_summary_chocolate, NumberFormat.getCurrencyInstance().format(chocAmount));
+        }
 
-        summaryMessage = summaryMessage + "\nThank you!";
+        summaryMessage += "\n\n" + getString(R.string.order_summary_total, NumberFormat.getCurrencyInstance().format(totalAmount));
+        summaryMessage += "\n\n" + getString(R.string.order_summary_thankyou);
 
         return summaryMessage;
 
